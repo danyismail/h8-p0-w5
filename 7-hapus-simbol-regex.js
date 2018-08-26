1c82c2819f224b06bbdca401423b68d115fec007
danyismail <dany_arie@yahoo.com>
2018-08-26 23:44:21 +0700

    hapus-symbol-regex

---
+++ 7-hapus-simbol-regex.js
@@ -0,0 +1,14 @@
+function hapusSimbol(str) {
+    var cariRegex = str.match(/[a-z0-9]+/g)
+    return cariRegex.join('')
+
+  }
+  
+  
+  
+  // TEST CASES
+  console.log(hapusSimbol('test%$4aa')); // test4aa
+  console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
+  console.log(hapusSimbol('ma@#k!an~')); // makan
+  console.log(hapusSimbol('coding')); // coding
+  console.log(hapusSimbol('1+3-5*2=100')); // 1352100
